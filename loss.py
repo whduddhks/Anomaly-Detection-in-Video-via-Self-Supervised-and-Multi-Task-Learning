e94a5b4ef921480a13a66f5dba604f702f1b4307
whduddhks <jyy1551@naver.com>
2022-07-07 16:13:39 +0900

    add softmax

--- loss.py
+++ loss.py
@@ -35,6 +35,23 @@ class mdloss(nn.Module):
     def __init__(self):
         super().__init__()
 
-    def forward(self, md_merged_output, target_merged):
+    def forward(self, md_yolo, md_res, target_yolo, target_res):
+        softmax = nn.Softmax(dim=1)
         loss = nn.L1Loss(reduction='mean')
+        md_yolo = softmax(md_yolo)
+        md_res = softmax(md_res)
+        target_yolo = softmax(target_yolo)
+        target_res = softmax(target_res)
+
+        md_merged_output = torch.cat((md_yolo, md_res), 1)
+        target_merged = torch.cat((target_yolo, target_res), 1)
         return loss(md_merged_output, target_merged)
+
+
+if __name__ == "__main__":
+    a = torch.randn([10, 80])
+    b = torch.randn([10, 1000])
+    c = torch.randn([10, 80])
+    d = torch.randn([10, 1000])
+    loss = mdloss()
+    print(loss(a, b, c, d))
