a101a59a739d9743901b13a32b9735a91a589a35
whduddhks <jyy1551@naver.com>
2022-07-07 22:24:10 +0900

    load image

--- conv_utils.py
+++ conv_utils.py
@@ -17,3 +17,17 @@ def weights_init_normal(self):
         elif isinstance(m, nn.Linear):
             nn.init.kaiming_uniform_(m.weight)
             nn.init.constant_(m.bias, 0)
+            
+
+def img_crop(clips, pred):
+    crop_img = []
+    for p in pred[0]:
+        crop_flow_img = []
+        for c in clips:
+            c = c.numpy()
+            p = p.int()
+            crop_flow = cv2.resize(c[p[1]:p[3], p[0]:p[2]], (64, 64)).astype('float32')
+            crop_flow /= 255
+            crop_flow_img.append(crop_flow)
+        crop_img.append(crop_flow_img)
+    return crop_img
