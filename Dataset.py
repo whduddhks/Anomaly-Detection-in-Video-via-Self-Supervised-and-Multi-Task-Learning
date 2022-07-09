6b854e877fbd20d4d620a8bfa9d460c970921c1c
whduddhks <jyy1551@naver.com>
2022-07-10 00:46:29 +0900

    add val dataloader

--- Dataset.py
+++ Dataset.py
@@ -18,25 +18,28 @@ class train_dataset(Dataset):
     
     def __init__(self, cfg):
         # list마다 각 folder의 이미지들이 넣어져있음
-        self.videos = []
-        self.all_frames = []
+        self.training_videos = []
+        self.all_frames_training = []
+
         # from cfg.train_data : 'data_root/' + dataset name + '/training/' + all folder
-        for folder in sorted(glob.glob(f'{cfg.train_data}/*')):
+        all_folder = sorted(glob.glob(f'{cfg.train_data}/*'))
+        all_folder_len = len(all_folder)
+        for folder in all_folder[:int(all_folder_len*0.85 + 1)]:
             # root 속에 있는 모든 jpg 파일들 선택
             all_imgs = glob.glob(f'{folder}/*.jpg')
             all_imgs.sort()
-            self.videos.append(all_imgs)
+            self.training_videos.append(all_imgs)
 
-            frames = list(range(3, len(all_imgs) - 2))
+            frames = list(range(3, len(all_imgs) - 3))
             random.shuffle(frames)
-            self.all_frames.append(frames)
-
+            self.all_frames_training.append(frames)
+        
     def __len__(self):  
         return len(self.videos)
 
     def __getitem__(self, idx):
         folder = self.videos[idx]
-        start = self.all_frames[idx][-1]
+        start = self.all_frames_training[idx][-1]
         i_path = folder[start]
 
         video_clip = []
@@ -54,7 +57,7 @@ class train_dataset(Dataset):
             else:
                 random_clip.append(np_load_frame(folder[temp - 1]))
                 temp -= 1
-        
+    
         random_clip.reverse()
 
         temp = start
@@ -73,3 +76,22 @@ class train_dataset(Dataset):
         return idx, video_clip, random_clip, i_path
 
 
+
+class val_dataset(Dataset):
+    
+    def __init__(self, cfg, video_folder):
+        self.imgs = glob.glob(video_folder + '/*.jpg')
+        self.imgs.sort()
+        self.img_idx = range(3, len(self.imgs)-3)
+        
+    def __len__(self):  
+        return len(self.imgs) - 6 
+
+    def __getitem__(self, idx):
+        video_clips = []
+        start = self.img_idx[idx] - 3
+        for i in range(7):
+            video_clips.append(self.imgs[start + i])
+        
+        video_clips = np.array(video_clips)
+        return video_clips
