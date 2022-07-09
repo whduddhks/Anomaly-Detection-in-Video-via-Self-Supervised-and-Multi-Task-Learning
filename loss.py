7374a6f1ad7429e403c4f43baab4a8c3306af1d6
whduddhks <jyy1551@naver.com>
2022-07-09 18:09:44 +0900

    detele test code;

--- loss.py
+++ loss.py
@@ -35,7 +35,7 @@ class mdloss(nn.Module):
     def __init__(self):
         super().__init__()
 
-    def forward(self, md_yolo, md_res, target_yolo, target_res):
+    def forward(self, md_res, md_yolo, target_res, target_yolo):
         softmax = nn.Softmax(dim=1)
         loss = nn.L1Loss(reduction='mean')
         md_yolo = softmax(md_yolo)
@@ -46,12 +46,3 @@ class mdloss(nn.Module):
         md_merged_output = torch.cat((md_yolo, md_res), 1)
         target_merged = torch.cat((target_yolo, target_res), 1)
         return loss(md_merged_output, target_merged)
-
-
-if __name__ == "__main__":
-    a = torch.randn([10, 80])
-    b = torch.randn([10, 1000])
-    c = torch.randn([10, 80])
-    d = torch.randn([10, 1000])
-    loss = mdloss()
-    print(loss(a, b, c, d))
